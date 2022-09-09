ab09246861dd3fd9e14af0965c28b57452b08f1c
Md Mustakin Mahi <63896978+Mustakin-140@users.noreply.github.com>
2022-09-09 21:20:18 +0600

    Add files via upload

---
+++ Basic_Calculator.py
@@ -0,0 +1,12 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+Add = int(num1) + int(num2)
+Sub = int(num1) - int(num2)
+Mul = int(num1) * int(num2)
+Div = int(num1) / int(num2)
+
+print(Add)
+print(Sub)
+print(Mul)
+print(Div)
---
+++ Mad_libs_game.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+dept = input("Enter your department: ")
+cgpa = input("Enter your cgpa: ")
+
+print("My name is " + name)
+print("My department is " + dept)
+print("My cgpa is " + cgpa)
