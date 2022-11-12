69d7c6d66c9e337d8d23b6dd4852bc7ce9457f74
Md Mustakin Mahi <63896978+Mustakin-140@users.noreply.github.com>
2022-11-12 22:35:38 +0600

    Add files via upload

---
+++ num-word.py
@@ -0,0 +1,13 @@
+phone = input("Enter phone number: ")
+
+digit_mapping = {
+
+    "1" : "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four"
+}
+output = ""
+for ch in phone:
+    output += digit_mapping.get(ch,"!") + " "
+print(output)
